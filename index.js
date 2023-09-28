09bb93c9abdf51cfe1cf8c016d6761cf82dd6bb8
Kaone Daniel-Choba <danielchobakaone@gmail.com>
2023-09-28 16:47:47 +0200

    model intergration

--- index.js
+++ index.js
@@ -1,52 +1,87 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const sql = require('mssql');
+const axios = require('axios'); // Import the axios library
+const bodyParser = require('body-parser'); // Import bodyParser to parse form data
 const app = express();
+const apiKey = 'VmQUomyZ9membhLVi6T9DMrCNRW2cUEd';
+// Use bodyParser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Database configuration
-const config = {
-  user: 'your_username',
-  password: 'your_password',
-  server: 'your_server_name.database.windows.net',
-  database: 'your_database_name',
-  options: {
-    encrypt: true, // Use this for Azure SQL Database
-  },
-};
+app.use(express.static('html'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 app.get('/', (req, res) => {
   // Serve your HTML file here
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/submit-form', (req, res) => {
-  const { firstName, lastName, email, phoneNumber /* other form fields */ } = req.body;
+app.post('/submit-form', async (req, res) => {
+  try {
+    // Extract user inputs from the form
+    const userInputs = req.body;
 
-  const query = `
-    INSERT INTO YourTableName (FirstName, LastName, Email, PhoneNumber, /* other columns */)
-    VALUES (@firstName, @lastName, @email, @phoneNumber, /* other values */);
+    const data= [{
+      "person_age": parseInt(userInputs.personAge),
+      "person_income": parseInt(userInputs.income),
+      "person_home_ownership": userInputs.homeOwner,
+      "person_emp_length": parseFloat(userInputs.employmentLength),
+      "loan_intent": userInputs.loanIntent,
+      "loan_grade": userInputs.loanGrade,
+      "loan_amnt": parseInt(userInputs.loanAmount),
+      "loan_int_rate": parseFloat(userInputs.interestRate),
+      "loan_status": 0, 
+      "loan_percent_income": parseFloat(userInputs.loanPercentIncome),
+      "cb_person_default_on_file": userInputs.cbPersonDefaultOnFile === 'true',
+      "cb_person_cred_hist_length": parseInt(userInputs.cbPersonCredHistLength)
+    }];
+
+    // Make an HTTP POST request to your machine learning model's scoring endpoint
+    const mlModelResponse = await axios.post(
+      'http://20.242.146.109:80/api/v1/service/loandesignermodel/score',
+       data , 
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          // Add any other headers required for authentication or authorization
+        },
+      }
+    );
+ 
+    // Handle the response from the machine learning model
+    const predictionsString = mlModelResponse.data;
+    const response = JSON.parse(predictionsString);
+    const predictions = response.result[0];
+
+    const loanStatus = predictions[12] === 1 ? "You qualify" : "You don't qualify";
+   
+    const predictioonScore = [predictions[13]];
+    
+    console.log(predictions)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Loan Prediction</title>
+    </head>
+    <body>
+      <h1>Loan Prediction Results</h1>
+      <p>Predicted Loan Status: ${loanStatus}</p>
+      <p>Predicted Accuracy Score: ${predictioonScore}</p>
+      <!-- Include other HTML content as needed -->
+    </body>
+    </html>
   `;
 
-  sql.connect(config)
-    .then((pool) => {
-      return pool.request()
-        .input('firstName', sql.NVarChar, firstName)
-        .input('lastName', sql.NVarChar, lastName)
-        .input('email', sql.NVarChar, email)
-        .input('phoneNumber', sql.NVarChar, phoneNumber)
-        /* Add inputs for other form fields */
-        .query(query);
-    })
-    .then(() => {
-      res.send('Form data saved successfully.');
-    })
-    .catch((err) => {
-      console.error('Error saving form data:', err);
-      res.status(500).send('Error saving form data.');
-    });
+  res.send(html);
+
+  } catch (error) {
+    console.error('Error making ML model request:', error);
+    res.status(500).json({ error: 'An error occurred while making the request to the ML model.' });
+  }
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
